cd8e2921a258c76ebfaf8e29272fcbcb509f47e9
leandroorlandin <31496728+leandroorlandin@users.noreply.github.com>
2023-03-20 11:13:58 -0300

    segundo commit via gitbash

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+import time
+import ast
+import os
+from pathlib import Path
+import csv
+import numpy as np
+import fnmatch
+
+
+# IMPRIME O HORÁRIO PARA CONTROLE
+def imprime_time():
+    localtime = time.localtime()
+    result = time.strftime("%I:%M:%S %p", localtime)
+    print(result)
+
+print("*********************************")
+print("***INICIO ***********************")
+print("*********************************")
+
+imprime_time()
+
+print("*********************************")
+print("***FIM **************************")
+print("*********************************")
+
+imprime_time()
+imprime_time()
+
